17028a7bd511058049fc4205c4fb68a6f4b5a363
Tarek <83262890+tarek102@users.noreply.github.com>
2022-11-22 18:03:24 +0200

    fix sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :entities, dependent: :destroy
+  has_many :groups, dependent: :destroy, foreign_key: 'author_id'
+  has_one_attached :avatar
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  ROLES = %i[admin default].freeze
+
+  def is?(requested_role)
+    role == requested_role.to_s
+  end
+
 end
