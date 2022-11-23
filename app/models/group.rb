7651c91e59598a5d00df4d7bddee229d3a71aa06
Tarek <83262890+tarek102@users.noreply.github.com>
2022-11-23 14:01:32 +0200

    adjust Models

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,14 @@
 class Group < ApplicationRecord
+  belongs_to :user, class_name: 'User'
+  has_many :group_entities, foreign_key: :entity_id, dependent: :destroy
+  has_many :entities, dependent: :destroy, through: :group_entities
+
+
+
+  validates :name, presence: true
+  validates :icon, presence: true
+
+  def total_expenses
+    entities.sum('amount')
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :entities, dependent: :destroy
+  has_many :entities, dependent: :destroy, foreign_key: 'author_id'
   has_many :groups, dependent: :destroy, foreign_key: 'author_id'
   has_one_attached :avatar
 
   validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :password, presence: true, length: { minimum: 6 }
 
   ROLES = %i[admin default].freeze
 
